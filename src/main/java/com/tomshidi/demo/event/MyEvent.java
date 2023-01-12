329629807e7950085c4e4577c810e921ab93b853
tangshili <tangshili@do1.com.cn>
2023-01-12 18:12:33 +0800

    增加自定义Event、EventListener用例

---
+++ src/main/java/com/tomshidi/demo/event/MyEvent.java
@@ -0,0 +1,13 @@
+package com.tomshidi.demo.event;
+
+import org.springframework.context.ApplicationEvent;
+
+/**
+ * @author tangshili
+ * @date 2023/1/12 17:04
+ */
+public class MyEvent extends ApplicationEvent {
+    public MyEvent(Object source) {
+        super(source);
+    }
+}
