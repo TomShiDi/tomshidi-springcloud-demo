2f9d41f196a04a4213fd379393c01225136ab12f
TomShiDi <1341109792@qq.com>
2023-03-13 09:53:42 +0800

    循环引用问题

--- core-demo/src/main/java/com/tomshidi/demo/circular/CircularReferenceA.java
+++ core-demo/src/main/java/com/tomshidi/demo/circular/CircularReferenceA.java
@@ -2,19 +2,22 @@ package com.tomshidi.demo.circular;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Lazy;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * @author tomshidi
  * @date 2022/1/23 16:12
  */
-//@Component
+@Component
 @Scope(scopeName = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CircularReferenceA {
     private CircularReferenceB circularReferenceB;
 
+    @Lazy
     @Autowired
-    public CircularReferenceA(CircularReferenceB circularReferenceB) {
+    public void setCircularReferenceB(CircularReferenceB circularReferenceB) {
         this.circularReferenceB = circularReferenceB;
     }
 }
--- core-demo/src/main/java/com/tomshidi/demo/circular/CircularReferenceB.java
+++ core-demo/src/main/java/com/tomshidi/demo/circular/CircularReferenceB.java
@@ -3,18 +3,19 @@ package com.tomshidi.demo.circular;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.context.annotation.Scope;
+import org.springframework.stereotype.Component;
 
 /**
  * @author tomshidi
  * @date 2022/1/23 16:13
  */
-//@Component
+@Component
 @Scope(scopeName = ConfigurableBeanFactory.SCOPE_SINGLETON)
 public class CircularReferenceB {
     private CircularReferenceA circularReferenceA;
 
     @Autowired
-    public CircularReferenceB(CircularReferenceA circularReferenceA) {
+    public void setCircularReferenceA(CircularReferenceA circularReferenceA) {
         this.circularReferenceA = circularReferenceA;
     }
 }
