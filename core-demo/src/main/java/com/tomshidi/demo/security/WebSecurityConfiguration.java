6ab02432985e8e70dd249eef3b9a5f5b35cdba5f
TomShiDi <1341109792@qq.com>
2025-11-19 23:03:01 +0800

    feature: fix old spring-security code

--- core-demo/src/main/java/com/tomshidi/demo/security/WebSecurityConfiguration.java
+++ core-demo/src/main/java/com/tomshidi/demo/security/WebSecurityConfiguration.java
@@ -5,7 +5,9 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.web.SecurityFilterChain;
 
 /**
  * @author tangshili
@@ -16,17 +18,11 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 public class WebSecurityConfiguration {
 
     @Bean
-    protected void filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable)
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-        http.authorizeHttpRequests()
-                .anyRequest()
-                .permitAll()
-//                .authenticated()
-                .and()
-                .headers()
-                .frameOptions()
-                .disable();
+    public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
+        return http.csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(smc -> smc.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(ahr -> ahr.anyRequest().permitAll())
+                .headers(hc -> hc.frameOptions(HeadersConfigurer.FrameOptionsConfig::disable))
+                .build();
     }
 }
