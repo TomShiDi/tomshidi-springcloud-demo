cc92b39c3ca1a6002349838c9fbb5d40fa0c32ae
TomShiDi <1341109792@qq.com>
2025-11-21 20:42:23 +0800

    feature: rename module and add ollama example

---
+++ ai-demo/ollama02/src/main/java/com/tomshidi/aidemo/ollama/SaaOllamaApplication.java
@@ -0,0 +1,11 @@
+package com.tomshidi.aidemo.ollama;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SaaOllamaApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SaaOllamaApplication.class, args);
+    }
+}
